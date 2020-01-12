a6b96be4f01a2e8915db102a386e970e366315a2
cstelios <58387932+cstelios@users.noreply.github.com>
2020-01-13 00:42:15 +0200

    v1

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessTxt;
+    [SerializeField] TextMeshProUGUI guessesTxt;
+    
+    int guess;
+    static int guesses = 1;
+
+    private void Start()
+    {
+        Guess();
+    }
+
+    public void Higher()
+    {
+        min = guess;
+        Guess();
+    }
+
+    public void Lower()
+    {
+        max = guess;
+        Guess();
+    }
+
+    void Guess()
+    {
+        guesses += 1;
+        guessesTxt.text = guesses.ToString();
+        guess = Random.Range(min, max + 1);
+        guessTxt.text = guess.ToString();
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if(currentSceneIndex == 2)
+        {
+            SceneManager.LoadScene(0);
+        } 
+        else
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
